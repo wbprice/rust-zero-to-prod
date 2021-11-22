82ce41ca2b621884705779a16edf941ef751006a
Blaine Price <blaine@ario.com>
2021-11-22 17:41:18 -0500

    bind a random port for tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::net::TcpListener;
+
 #[async_std::main]
 async fn main() -> Result<(), tide::Error> {
-    zero2prod::run("localhost:8080").await?;
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Unable to bind port");
+    zero2prod::run(listener).await?;
     Ok(())
 }
--- tests/health.rs
+++ tests/health.rs
@@ -1,13 +1,13 @@
+use std::net::TcpListener;
 use zero2prod;
 
 #[async_std::test]
 async fn health_check() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://localhost:8080/health")
+        .get(format!("{}/health", address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -16,6 +16,11 @@ async fn health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let _ = async_std::task::spawn(zero2prod::run("localhost:8080"));
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener);
+    let _ = async_std::task::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }

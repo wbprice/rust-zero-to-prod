b3b03c4abf2c705283b57623de34273b1dac759e
Blaine Price <blaine@ario.com>
2021-11-29 17:21:02 -0500

    man

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use tide::{Request, Response, Result, StatusCode};
 use sqlx::types::{chrono::Utc, Uuid};
 use sqlx::Acquire;
 use tide_sqlx::SQLxRequestExt;
+use tracing_futures::Instrument;
 
 #[derive(serde::Deserialize, Debug)]
 struct FormData {
@@ -12,10 +13,23 @@ struct FormData {
 }
 
 pub async fn subscribe(mut req: Request<()>) -> Result {
+    tracing::info!("A new subscription request");
     if let Ok(result) = req.body_form().await {
-        let mut pg_conn = req.sqlx_conn::<Postgres>().await;
         let new_sub: FormData = result;
+        let request_id = Uuid::new_v4();
+        let request_span = tracing::info_span!(
+            "Adding a new subscriber",
+            %request_id,
+            subscriber_email = %new_sub.email,
+            subscriber_name = %new_sub.name
+        );
+        let _request_span_guard = request_span.enter();
 
+        let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
+        // Get the connection
+        let mut pg_conn = req.sqlx_conn::<Postgres>().await;
+        // Run the query
         match sqlx::query!(
             r#"
             insert into subscriptions (id, email, name, subscribed_at)  
@@ -27,15 +41,20 @@ pub async fn subscribe(mut req: Request<()>) -> Result {
             Utc::now()
         )
         .execute(pg_conn.acquire().await?)
+        .instrument(query_span)
         .await
         {
-            Ok(_) => Ok(Response::new(StatusCode::Ok)),
+            Ok(_) => {
+                tracing::info!("Saved successfully");
+                Ok(Response::new(StatusCode::Ok))
+            }
             Err(e) => {
-                println!("Failed to execute query: {}", e);
+                tracing::error!("failed to execute query: {:?}", e);
                 Ok(Response::new(StatusCode::BadRequest))
             }
         }
     } else {
+        tracing::error!("the request was badly formatted");
         Ok(Response::new(StatusCode::BadRequest))
     }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,17 @@
 use crate::routes::{health_check, subscribe};
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tide::log::LogMiddleware;
 use tide::prelude::*;
 use tide_sqlx::SQLxMiddleware;
 
 pub async fn run(listener: TcpListener, pool: PgPool) -> tide::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let mut app = tide::new();
     app.with(SQLxMiddleware::from(pool));
+    app.with(LogMiddleware::new());
     app.at("/health").get(health_check);
     app.at("/subscriptions").post(subscribe);
     let mut listener = app.bind(listener).await?;

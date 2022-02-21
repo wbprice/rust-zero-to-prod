ea37ced03d6819c9584679d8b0906b15ff26f0f4
Blaine Price <blaine@ario.com>
2022-02-20 19:36:13 -0500

    Implement Observability

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,41 +1,64 @@
-use sqlx::Postgres;
-use tide::{Request, Response, Result, StatusCode};
-
+use async_std::sync::RwLockWriteGuard;
 use sqlx::types::{chrono::Utc, Uuid};
 use sqlx::Acquire;
-use tide_sqlx::SQLxRequestExt;
+use sqlx::Postgres;
+use tide::{Request, Response, StatusCode};
+use tide_sqlx::{ConnectionWrapInner, SQLxRequestExt};
+use tracing::{field, Span};
 
 #[derive(serde::Deserialize, Debug)]
-struct FormData {
+pub struct FormData {
     name: String,
     email: String,
 }
 
-pub async fn subscribe(mut req: Request<()>) -> Result {
-    if let Ok(result) = req.body_form().await {
-        let mut pg_conn = req.sqlx_conn::<Postgres>().await;
-        let new_sub: FormData = result;
-
-        match sqlx::query!(
-            r#"
+#[tracing::instrument(name = "Saving subscriber details in the database", skip(pool, form))]
+pub async fn insert_subscriber(
+    mut pool: RwLockWriteGuard<'_, ConnectionWrapInner<Postgres>>,
+    form: FormData,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
             insert into subscriptions (id, email, name, subscribed_at)  
             values ($1, $2, $3, $4)
             "#,
-            Uuid::new_v4(),
-            new_sub.email,
-            new_sub.name,
-            Utc::now()
-        )
-        .execute(pg_conn.acquire().await?)
-        .await
-        {
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.acquire().await?)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(req),
+    fields(
+        subscriber_email = field::Empty,
+        subscriber_name = field::Empty
+    )
+)]
+pub async fn subscribe(mut req: Request<()>) -> tide::Result {
+    if let Ok(result) = req.body_form().await {
+        let pg_conn = req.sqlx_conn::<Postgres>().await;
+        let new_sub: FormData = result;
+
+        let span = Span::current();
+        span.record("subscriber_email", &new_sub.email.as_str());
+        span.record("subscriber_name", &new_sub.name.as_str());
+
+        match insert_subscriber(pg_conn, new_sub).await {
             Ok(_) => Ok(Response::new(StatusCode::Ok)),
-            Err(e) => {
-                println!("Failed to execute query: {}", e);
-                Ok(Response::new(StatusCode::BadRequest))
-            }
+            Err(_) => Ok(Response::new(StatusCode::BadRequest)),
         }
     } else {
+        tracing::error!("Couldn't parse input");
         Ok(Response::new(StatusCode::BadRequest))
     }
 }

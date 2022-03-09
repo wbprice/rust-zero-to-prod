19f02f6c454f1fa19d7c595647fa0b017df78aeb
Blaine Price <blaine@ario.com>
2022-03-09 17:17:58 -0500

    oh man

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,8 @@
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use async_std::sync::Arc;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tide::prelude::*;
@@ -10,6 +12,35 @@ use tide_tracing::TraceMiddleware;
 #[derive(Clone)]
 pub struct State {
     pub email_client: Arc<EmailClient>,
+}
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    // create database connection pool
+    let pg_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+
+    // Configure email client
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender address");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    // fetch port from configuration file
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address).expect("Couldn't bind port");
+
+    run(listener, pg_pool, email_client)
 }
 
 pub async fn run(

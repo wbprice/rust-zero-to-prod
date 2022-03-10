a7a401a684e0db87384685299aba96bfc4ec4d7a
Blaine Price <blaine@ario.com>
2022-03-10 17:50:58 -0500

    begin subscription confirmation

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,7 +1,17 @@
 use crate::startup::State;
+use fake::faker::lorem::raw::Paragraphs;
+use serde::Deserialize;
 use tide::{Request, Response, Result, StatusCode};
+
+#[derive(serde::Deserialize)]
+pub struct Parameters {
+    subscription_token: String
+} 
 
 #[tracing::instrument(name = "Confirm a pending subscriber", skip(req))]
 pub async fn confirm(mut req: Request<State>) -> Result {
+    let params: Parameters = req.query()?;
+
     Ok(Response::new(StatusCode::Ok))
 }
+

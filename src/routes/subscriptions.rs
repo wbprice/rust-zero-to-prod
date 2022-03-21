b28f937191dc5222192c4b9759ab87a63139c7ca
Blaine Price <blaine@ario.com>
2022-03-21 17:52:03 -0400

    Custom error class for storing token errors

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -23,9 +23,22 @@ impl std::fmt::Display for StoreTokenError {
 
 impl actix_web::ResponseError for StoreTokenError {}
 
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
+
 impl std::fmt::Debug for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\nCaused by:\n\t{}", self, self.0)
+        error_chain_fmt(self, f)
     }
 }
 
@@ -181,8 +194,6 @@ pub async fn store_token(
     )
     .execute(transaction)
     .await
-    .map_err(|e| {
-        StoreTokenError(e)
-    })?;
+    .map_err(|e| StoreTokenError(e))?;
     Ok(())
 }
